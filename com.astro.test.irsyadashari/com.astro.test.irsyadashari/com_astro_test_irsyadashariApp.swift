97016f45cc9acfba2b129b7290136dc43e5b7384
Irsyad Ashari <36505012+irsyadashari@users.noreply.github.com>
2025-10-18 08:03:50 +0700

    Adding initial project files

---
+++ com.astro.test.irsyadashari/com.astro.test.irsyadashari/com_astro_test_irsyadashariApp.swift
@@ -0,0 +1,20 @@
+//
+//  com_astro_test_irsyadashariApp.swift
+//  com.astro.test.irsyadashari
+//
+//  Created by Muh Irsyad Ashari on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct com_astro_test_irsyadashariApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
